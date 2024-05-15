872a1d80c992a73f2b1e6b8b62b74b40d6119025
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-05-15 16:59:40 +0300

    reset

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,23 +1,21 @@
-import { Link, useLocation } from "react-router-dom";
-import s from "./MovieList.module.css";
+import { Link, useLocation } from 'react-router-dom';
+import s from './MovieList.module.css'
 
-const MovieList = ({ movie }) => {
+
+const MovieList = ({movie}) => {
   const location = useLocation();
   return (
-    <li className={s.item}>
-      <div className={s.film_card}>
-        <Link
-          to={`${movie.id.toString()}`}
-          state={location}
-        >
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt={`${movie.title}`}
-          />
+      <li className={s.item}>
+        <Link to={`${movie.id.toString()}`} state={location}>
+          <div className={s.film_card}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={`${movie.title}`}
+            />
+            <h3>{movie.title}</h3>
+          </div>
         </Link>
-        <h3>{movie.title}</h3>
-      </div>
-    </li>
+      </li>
   );
 };
 
