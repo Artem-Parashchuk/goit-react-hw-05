12ab5c89f826ed622f0cd5231fdaea4653f0b80f
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-05-15 21:38:44 +0300

    fix home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,56 +1,53 @@
 import { useEffect, useState } from "react";
 import { fetchTrendingTodayMovies } from "../../services/api";
-import { Link } from "react-router-dom";
 
 import s from "./HomePage.module.css";
 import { InfinitySpin } from "react-loader-spinner";
+import MovieList from "../../components/MovieList/MovieList";
 
 const HomePage = () => {
   const [films, setFilms] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getFilms = async () => {
-      const res = await fetchTrendingTodayMovies();
-      setFilms(res);
+      try {
+        setError(false);
+        setLoading(true)
+        const res = await fetchTrendingTodayMovies();
+        setFilms(res);
+      } catch (error) {
+        console.log(error.message);
+        setError(true);
+      }finally{
+        setLoading(false);
+      }
     };
     getFilms();
   }, []);
 
-  if (!films) {
-    return (<div className={s.loader}>
-      <InfinitySpin
-        visible={true}
-        width="200"
-        color="rgb(0, 0, 118)"
-        ariaLabel="infinity-spin-loading"
-      />
-    </div>
+  if (loading) {
+    return (
+      <div className={s.loader}>
+        <InfinitySpin
+          visible={true}
+          width="200"
+          color="rgb(0, 0, 118)"
+          ariaLabel="infinity-spin-loading"
+        />
+      </div>
     );
   }
 
+  if (error) {
+    return <h2 className={s.error}>Щось пішло не так, ми працюємо над цим</h2>;
+  }
+
   return (
-  
     <div className={s.home}>
       <h1 className={s.title}>Trending today</h1>
-      <ul className={s.list}>
-        {films.map((film) => {
-          return (
-            <li
-              key={film.id}
-              className={s.item}
-            >
-              <Link to={`movies/${film.id.toString()}`}>
-                <div className={s.film_card}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`}
-                    alt={`${film.title}`}
-                  />
-                  <h3>{film.title}</h3>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <MovieList films={films}/>
     </div>
   );
 };
